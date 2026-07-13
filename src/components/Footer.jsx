bdaba516ae96f1895b1c2b46a3468a866780e674
Malik-Maharjan <malikmaharjan@gmail.com>
2026-07-13 19:45:22 +0545

    updated hero

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-20 border-t  border-gray-100 lg:max-w-7xl mx-auto">
+    <div className="mt-5 border-t  border-gray-100 lg:max-w-7xl mx-auto">
       <h1 className="py-3 px-5 sm:py-5 xl:px-0 text-white font-bold text-2xl sm:text-3xl lg:text-4xl">Portfolio</h1>
     </div>
   );
